332eb2e96b99c3a2c702e18a92b49f66da18528a
i22prpaj <i22prpaj@uco.es>
2024-11-24 10:21:52 +0000

    modified:   build/CMakeCache.txt 	new file:   build/CMakeFiles/CMakeDirectoryInformation.cmake 	new file:   build/CMakeFiles/Makefile.cmake 	new file:   build/CMakeFiles/Makefile2 	new file:   build/CMakeFiles/TargetDirectories.txt 	new file:   build/CMakeFiles/progress.marks 	new file:   build/Makefile 	new file:   build/cmake_install.cmake 	new file:   build/src/CMakeFiles/CMakeDirectoryInformation.cmake 	new file:   build/src/CMakeFiles/progress.marks 	new file:   build/src/Makefile 	new file:   build/src/admin/CMakeFiles/CMakeDirectoryInformation.cmake 	new file:   build/src/admin/CMakeFiles/admin.dir/DependInfo.cmake 	new file:   build/src/admin/CMakeFiles/admin.dir/build.make 	new file:   build/src/admin/CMakeFiles/admin.dir/cmake_clean.cmake 	new file:   build/src/admin/CMakeFiles/admin.dir/cmake_clean_target.cmake 	new file:   build/src/admin/CMakeFiles/admin.dir/depend.make 	new file:   build/src/admin/CMakeFiles/admin.dir/flags.make 	new file:   build/src/admin/CMakeFiles/admin.dir/link.txt 	new file:   build/src/admin/CMakeFiles/admin.dir/progress.make 	new file:   build/src/admin/CMakeFiles/progress.marks 	new file:   build/src/admin/Makefile 	new file:   build/src/admin/cmake_install.cmake 	new file:   build/src/alumno/CMakeFiles/CMakeDirectoryInformation.cmake 	new file:   build/src/alumno/CMakeFiles/alumno.dir/DependInfo.cmake 	new file:   build/src/alumno/CMakeFiles/alumno.dir/build.make 	new file:   build/src/alumno/CMakeFiles/alumno.dir/cmake_clean.cmake 	new file:   build/src/alumno/CMakeFiles/alumno.dir/cmake_clean_target.cmake 	new file:   build/src/alumno/CMakeFiles/alumno.dir/depend.make 	new file:   build/src/alumno/CMakeFiles/alumno.dir/flags.make 	new file:   build/src/alumno/CMakeFiles/alumno.dir/link.txt 	new file:   build/src/alumno/CMakeFiles/alumno.dir/progress.make 	new file:   build/src/alumno/CMakeFiles/progress.marks 	new file:   build/src/alumno/Makefile 	new file:   build/src/alumno/cmake_install.cmake 	new file:   build/src/cmake_install.cmake 	new file:   build/src/profesor/CMakeFiles/CMakeDirectoryInformation.cmake 	new file:   build/src/profesor/CMakeFiles/profesor.dir/DependInfo.cmake 	new file:   build/src/profesor/CMakeFiles/profesor.dir/build.make 	new file:   build/src/profesor/CMakeFiles/profesor.dir/cmake_clean.cmake 	new file:   build/src/profesor/CMakeFiles/profesor.dir/cmake_clean_target.cmake 	new file:   build/src/profesor/CMakeFiles/profesor.dir/depend.make 	new file:   build/src/profesor/CMakeFiles/profesor.dir/flags.make 	new file:   build/src/profesor/CMakeFiles/profesor.dir/link.txt 	new file:   build/src/profesor/CMakeFiles/profesor.dir/progress.make 	new file:   build/src/profesor/CMakeFiles/progress.marks 	new file:   build/src/profesor/Makefile 	new file:   build/src/profesor/cmake_install.cmake 	new file:   build/src/usuario/CMakeFiles/CMakeDirectoryInformation.cmake 	new file:   build/src/usuario/CMakeFiles/progress.marks 	new file:   build/src/usuario/CMakeFiles/usuario.dir/DependInfo.cmake 	new file:   build/src/usuario/CMakeFiles/usuario.dir/build.make 	new file:   build/src/usuario/CMakeFiles/usuario.dir/cmake_clean.cmake 	new file:   build/src/usuario/CMakeFiles/usuario.dir/cmake_clean_target.cmake 	new file:   build/src/usuario/CMakeFiles/usuario.dir/depend.make 	new file:   build/src/usuario/CMakeFiles/usuario.dir/flags.make 	new file:   build/src/usuario/CMakeFiles/usuario.dir/link.txt 	new file:   build/src/usuario/CMakeFiles/usuario.dir/progress.make 	new file:   build/src/usuario/Makefile 	new file:   build/src/usuario/cmake_install.cmake 	modified:   src/usuario/CMakeLists.txt

---
+++ build/src/profesor/CMakeFiles/profesor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/profesor.dir/profesor.cc.o"
+  "libprofesor.a"
+  "libprofesor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/profesor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
