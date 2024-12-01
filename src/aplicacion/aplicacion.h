33b84ab1c315ccc2269b012c731590d61d6872e7
i22prpaj <i22prpaj@uco.es>
2024-12-01 11:06:55 +0000

    Add user validation and improve output formatting in main application

--- src/aplicacion/aplicacion.h
+++ src/aplicacion/aplicacion.h
@@ -6,6 +6,7 @@
 #include "admin.h"
 #include <fstream>
 #include <sstream>
+#include <stdio.h>
 
 void cargarBD(std::vector<Alumno>& alumnos, std::vector<Profesor>& profesores, std::vector<Admin>& admins);
 void guardarBD(const std::vector<Alumno>& alumnos, const std::vector<Profesor>& profesores, const std::vector<Admin>& admins);
@@ -13,5 +14,6 @@ void guardarBD(const std::vector<Alumno>& alumnos, const std::vector<Profesor>& 
 bool Registrarse(std::vector<Alumno>& alumnos, std::vector<Profesor>& profesores, std::vector<Admin>& admins);
 bool IniciarSesion(const std::vector<Alumno>& alumnos, const std::vector<Profesor>& profesores, const std::vector<Admin>& admins);
 
-
+bool existeusuario(std::string nombreusuario, std::string contrasena,const std::vector<Alumno>& alumnos, const std::vector<Profesor>& profesores, const std::vector<Admin>& admins);
+bool validarDNI(const std::string& dni);
 #endif
